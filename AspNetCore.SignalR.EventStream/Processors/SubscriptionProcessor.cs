6f47a8bb67c0a560e48d5ad582a2c5e72fd89b87
Shantanu <shantanu_negi@hotmail.com>
2022-08-26 22:11:52 +1000

    added any check

--- AspNetCore.SignalR.EventStream/Processors/SubscriptionProcessor.cs
+++ AspNetCore.SignalR.EventStream/Processors/SubscriptionProcessor.cs
@@ -57,76 +57,79 @@ namespace AspNetCore.SignalR.EventStream.Processors
                     {
                         var activeSubscriptions = await _repository.GetActiveSubscriptionsAsync();
 
-                        Parallel.ForEach(activeSubscriptions, new ParallelOptions { MaxDegreeOfParallelism = MaxDegreeOfParallelism }, async (subscription) =>
+                        if (activeSubscriptions.Any())
                         {
-                            try
+                            Parallel.ForEach(activeSubscriptions, new ParallelOptions { MaxDegreeOfParallelism = MaxDegreeOfParallelism }, async (subscription) =>
                             {
-                                var subscriber = await _repository.GetSubscriberAsync(subscription.SubscriptionId);
-
-                                if (subscriber == null)
+                                try
                                 {
-                                    return;
-                                }
+                                    var subscriber = await _repository.GetSubscriberAsync(subscription.SubscriptionId);
 
-                                //_logger?.LogInformation($"LastAccessedEventId: {subscriber.LastAccessedEventId}.");
-
-                                var subsciptionWithEvents = await _repository.GetSubscriberAsync(subscription.SubscriptionId,
-                                                                                subscriber.LastAccessedCurrentEventId > subscriber.LastAccessedFromEventId ?
-                                                                                    subscriber.LastAccessedCurrentEventId : subscriber.LastAccessedFromEventId, subscriber.LastAccessedToEventId);
-
-                                if (subsciptionWithEvents != null)
-                                {
-                                    if (subsciptionWithEvents.Stream.Events != null && subsciptionWithEvents.Stream.Events.Any())
+                                    if (subscriber == null)
                                     {
-                                        _logger?.LogInformation($"Streaming events ({subsciptionWithEvents.Stream.Events.Count()}) to subscriber {subscriber.SubscriberId}.");
+                                        return;
+                                    }
 
-                                        var eventSubscriberModel = new EventStreamSubscriberModelResult
+                                    //_logger?.LogInformation($"LastAccessedEventId: {subscriber.LastAccessedEventId}.");
+
+                                    var subsciptionWithEvents = await _repository.GetSubscriberAsync(subscription.SubscriptionId,
+                                                                                    subscriber.LastAccessedCurrentEventId > subscriber.LastAccessedFromEventId ?
+                                                                                        subscriber.LastAccessedCurrentEventId : subscriber.LastAccessedFromEventId, subscriber.LastAccessedToEventId);
+
+                                    if (subsciptionWithEvents != null)
+                                    {
+                                        if (subsciptionWithEvents.Stream.Events != null && subsciptionWithEvents.Stream.Events.Any())
                                         {
-                                            ConnectionId = subsciptionWithEvents.ConnectionId,
-                                            CreatedAt = subsciptionWithEvents.CreatedAt,
-                                            ReceiveMethod = subsciptionWithEvents.ReceiveMethod,
-                                            StreamId = subsciptionWithEvents.StreamId,
-                                            SubscriberId = subsciptionWithEvents.SubscriberId,
-                                            LastAccessedEventId = subsciptionWithEvents.LastAccessedFromEventId,
-                                            Stream = new EventStreamModelResult
+                                            _logger?.LogInformation($"Streaming events ({subsciptionWithEvents.Stream.Events.Count()}) to subscriber {subscriber.SubscriberId}.");
+
+                                            var eventSubscriberModel = new EventStreamSubscriberModelResult
                                             {
-                                                Name = subsciptionWithEvents.Stream.Name,
-                                                Events = subsciptionWithEvents.Stream.Events.Select(x => new EventModelResult
+                                                ConnectionId = subsciptionWithEvents.ConnectionId,
+                                                CreatedAt = subsciptionWithEvents.CreatedAt,
+                                                ReceiveMethod = subsciptionWithEvents.ReceiveMethod,
+                                                StreamId = subsciptionWithEvents.StreamId,
+                                                SubscriberId = subsciptionWithEvents.SubscriberId,
+                                                LastAccessedEventId = subsciptionWithEvents.LastAccessedFromEventId,
+                                                Stream = new EventStreamModelResult
                                                 {
-                                                    Data = x.Data,
-                                                    Id = x.Id,
-                                                    EventId = x.EventId,
-                                                    IsJson = x.IsJson,
-                                                    JsonData = x.JsonData,
-                                                    MetaData = x.MetaData,
-                                                    StreamId = subsciptionWithEvents.Stream.StreamId,
-                                                    StreamName = subsciptionWithEvents.Stream.Name,
-                                                    OriginalEventId = x.OriginalEventId,
-                                                    Type = x.Type,
-                                                    CreatedAt = x.CreatedAt
-                                                }).ToList(),
-                                                CreatedAt = subsciptionWithEvents.Stream.CreatedAt,
-                                                StreamId = subsciptionWithEvents.Stream.StreamId
-                                            }
-                                        };
+                                                    Name = subsciptionWithEvents.Stream.Name,
+                                                    Events = subsciptionWithEvents.Stream.Events.Select(x => new EventModelResult
+                                                    {
+                                                        Data = x.Data,
+                                                        Id = x.Id,
+                                                        EventId = x.EventId,
+                                                        IsJson = x.IsJson,
+                                                        JsonData = x.JsonData,
+                                                        MetaData = x.MetaData,
+                                                        StreamId = subsciptionWithEvents.Stream.StreamId,
+                                                        StreamName = subsciptionWithEvents.Stream.Name,
+                                                        OriginalEventId = x.OriginalEventId,
+                                                        Type = x.Type,
+                                                        CreatedAt = x.CreatedAt
+                                                    }).ToList(),
+                                                    CreatedAt = subsciptionWithEvents.Stream.CreatedAt,
+                                                    StreamId = subsciptionWithEvents.Stream.StreamId
+                                                }
+                                            };
 
-                                        await _eventStreamHubClient.SendAsync(eventSubscriberModel);
+                                            await _eventStreamHubClient.SendAsync(eventSubscriberModel);
 
-                                        _logger?.Log(
-                                            LogLevel.Information,
-                                            1000,
-                                            $"Finished streaming events ({subsciptionWithEvents.Stream.Events.Count()}) to subscriber {subscriber.SubscriberId}.",
-                                            null,
-                                            null);
+                                            _logger?.Log(
+                                                LogLevel.Information,
+                                                1000,
+                                                $"Finished streaming events ({subsciptionWithEvents.Stream.Events.Count()}) to subscriber {subscriber.SubscriberId}.",
+                                                null,
+                                                null);
+                                        }
                                     }
                                 }
-                            }
-                            catch (Exception ex)
-                            {
-                                _logger?.LogError(ex, $"Error in {Name} thread.");
-                                return;
-                            }
-                        });
+                                catch (Exception ex)
+                                {
+                                    _logger?.LogError(ex, $"Error in {Name} thread.");
+                                    return;
+                                }
+                            });
+                        }                        
                     }
                 }
                 catch (Exception ex)
